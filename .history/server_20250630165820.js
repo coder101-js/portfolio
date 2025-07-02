d55cbb2cb1809062edb11f134963338d1ca18291
='coder101_js <='op422010@gmail.com>
2025-07-03 02:31:40 +0500

    server

--- .history/server_20250630165820.js
+++ .history/server_20250630165820.js
@@ -10,7 +10,7 @@ const index = require('./controller/index');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // === Middlewares ===
 app.use(cors());
