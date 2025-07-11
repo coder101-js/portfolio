c590b026900f4417269c299ca8bbc798a6adad56
='coder101_js <='op422010@gmail.com>
2025-07-11 19:20:58 +0500

    sever

--- server.js
+++ server.js
@@ -14,14 +14,8 @@ const PORT = process.env.PORT || 5000;
 
 // === Middlewares ===
 
-const corsOptions = {
-  origin: "https://portfolio.buttnetworks.com",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
+app.use(cors())
 
-app.use(cors(corsOptions));
 
 
 app.use(express.json());
