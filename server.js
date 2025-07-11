2da9700706c2d377157bfb342ca0dee76f30ff55
='coder101_js <='op422010@gmail.com>
2025-07-11 18:34:51 +0500

    sever

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: 'https://portfolio.buttnetworks.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: false,
+  credentials: true,
 }));
 app.use(express.json());
 
