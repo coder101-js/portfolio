18fc6366becf023e94abc19c2b937d2c0adef129
='coder101_js <='op422010@gmail.com>
2025-07-11 18:22:24 +0500

    sever

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // === Middlewares ===
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://portfolio.buttnetworks.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: false,
+}));
 app.use(express.json());
 
 // Bot protection middleware
