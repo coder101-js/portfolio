8408aae89625e6da81dd87836633d12a81c3edfe
='coder101_js <='op422010@gmail.com>
2025-07-11 19:38:49 +0500

    sever

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const contact = require("./controller/contact");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 // === Middlewares ===
 
